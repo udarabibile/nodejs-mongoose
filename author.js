7fb5b94eca7158d83fe218b08f092741c9afe272
Udara Bibile <bibile.13@cse.mrt.ac.lk>
2020-06-22 23:16:14 +0530

    schema change

--- author.js
+++ author.js
@@ -1,9 +1,72 @@
 var mongoose = require('mongoose');
+var Book = require('./book');
  
 var AuthorSchema = mongoose.Schema({
     name: { type: String, required: true},
     books: [{type : mongoose.Schema.ObjectId, ref : 'Book'}],
 });
+
+AuthorSchema.post('save', async (doc, next) => {
+    const author = doc;
+    console.log(`AuthorSchema middleware on post save for author: ${author._id}`);
+    console.log(`Updating books for ids: ${author.books}`);
+
+    const books_status = await Book.updateMany({ _id: { $in: author.books } }, { $push: { 'authors': author._id}}); // , { 'new': true }
+    console.log(`Updating status: ${JSON.stringify(books_status)}`);
+
+    next();
+});
+
+AuthorSchema.pre('findOneAndUpdate', function(next) {
+    console.log('findOneAndUpdate pre hook');
+    console.log(this._update);
+
+    next();
+});
+
+AuthorSchema.post('findOneAndUpdate', function(doc) {
+    console.log('findOneAndUpdate post hook');
+
+    const originalBookIds = doc.books;
+    const updatedBookIds = this._update['$set'].books;
+
+    const addedBookIds = updatedBookIds.filter(b => !originalBookIds.includes(b));
+    const removedBookIds = originalBookIds.filter(b => !updatedBookIds.includes(b));
+
+    Promise.all(
+        addedBookIds.map(async id => await Book.findByIdAndUpdate(id, { $addToSet: { authors: doc._id }})),
+        removedBookIds.map(async id => await Book.findByIdAndUpdate(id, { $pull: { authors: doc._id }}))
+    );
+
+    // next();
+});
+
+AuthorSchema.post('findOneAndDelete', function(doc) {
+    console.log('findOneAndDelete post hook');
+
+    const originalBookIds = doc.books;
+    Promise.all(
+        originalBookIds.map(async id => await Book.findByIdAndUpdate(id, { $pull: { authors: doc._id }}))
+    );
+
+    // next();
+});
+
+
+AuthorSchema.pre('updateOne', async (doc, next) => {
+    console.log('updateOne pre hook');
+    console.log(this);
+    next();
+});
+
+AuthorSchema.post('updateOne', async (doc, next) => {
+    console.log('updateOne post hook');
+    console.log(doc);
+    next();
+});
+
+
+
 
 var Author = mongoose.model('Author', AuthorSchema);
 
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
+var assert = require('assert');
 
 var Author = require('./author');
 var Book = require('./book');
-var AuthorRepository = require('./AuthorRepository');
 
 const cleanUp = async () => {
     console.log('DROPPING COLLECTIONS!');
@@ -16,34 +16,103 @@ const printCollections = async () => {
 }
 
 (async () => {
-    mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+    mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
     await cleanUp();    
 
     console.log('CREATING BOOKS!');
-    const book1_doc = await Book.create({ title: 'book1' });
-    const book2_doc = await Book.create({ title: 'book2' });
-    const book3_doc = await Book.create({ title: 'book3' });
+    const book1_doc = await Book.create({ title: 'book1' }); console.log(book1_doc._id);
+    const book2_doc = await Book.create({ title: 'book2' }); console.log(book2_doc._id);
+    const book3_doc = await Book.create({ title: 'book3' }); console.log(book3_doc._id);
 
 
     console.log('CREATE AUTHOR!');
-    const author1_create = await AuthorRepository.create({ name: 'author1', books: [ book1_doc._id, book2_doc._id ] });
+    const author1_create = await Author.create({ name: 'author1', books: [ book1_doc._id, book2_doc._id ] });
     console.log(`Author created: ${author1_create._id}`);
-    await printCollections();
-
+    await assertAuthorCreate(author1_create);
 
     console.log('READ AUTHOR!');
-    console.log(`Authors:\n${await AuthorRepository.read(author1_create._id)}\n`);
+    // console.log(`Authors:\n${await Author.find({}).populate('books')}\n`);
 
 
     console.log('UPDATE AUTHOR!');
-    const author1_updated = await AuthorRepository.update({ _id: author1_create._id, name: 'author1_updated', books: [ book3_doc._id, book2_doc._id ] });
+    const author1_updated = await Author.findOneAndUpdate({ _id: author1_create._id }, { name: 'author1_updated', books: [ book3_doc._id, book2_doc._id ] });
+    // const author1_updated = await Author.updateOne({ _id: author1_create._id }, { name: 'author1_updated', books: [ book3_doc._id, book2_doc._id ] });
     console.log(`Author updated: ${author1_updated._id}`);
-    await printCollections();
+    await assertAuthorUpdate(author1_updated); // author1_updated is original document before update! Use { new: true } for new document.
 
 
     console.log('DELETE AUTHOR!');
-    const author1_deleted = await AuthorRepository.delete(author1_create._id);
-    console.log(author1_deleted);
+    const author1_deleted = await Author.findOneAndDelete({ _id: author1_create._id }); // Return original document.
     console.log(`Author deleted: ${author1_deleted._id}`);
-    await printCollections();
+    await assertAuthorDelete(author1_deleted);
+
+
+    // console.log('CREATE BOOK!');
+    // const book1_doc = await Book.create({ title: 'book1', authors });
+    // await printCollections();
 })();
+
+const fetchCollections = async (isPrint) => {
+    const authors = await Author.find({});
+    const books = await Book.find({});
+
+    if (isPrint) {
+        console.log(`Authors:\n${authors}\n`);
+        console.log(`Books:\n${books}\n`);
+    }
+    return { authors, books };
+}
+
+const assertAuthorCreate = async (author) => {
+    const { authors, books } =  await fetchCollections();
+
+    const author1 = authors.find(a => a._id.toString() === author._id.toString());
+
+    const book1 = books.find(b => b.title === 'book1');
+    const book2 = books.find(b => b.title === 'book2');
+    const book3 = books.find(b => b.title === 'book3');
+
+    assert(author1.books.includes(book1._id) === true);
+    assert(author1.books.includes(book2._id) === true);
+    assert(author1.books.includes(book3._id) === false);
+    assert(author1.name === author.name);
+
+    assert(book1.authors.includes(author1._id) === true);
+    assert(book2.authors.includes(author1._id) === true);
+    assert(book3.authors.includes(author1._id) === false);
+}
+
+const assertAuthorUpdate = async (author) => {
+    const { authors, books } = await fetchCollections();
+
+    const author1 = authors.find(a => a._id.toString() === author._id.toString());
+
+    const book1 = books.find(b => b.title === 'book1');
+    const book2 = books.find(b => b.title === 'book2');
+    const book3 = books.find(b => b.title === 'book3');
+
+    assert(author1.books.includes(book1._id) === false);
+    assert(author1.books.includes(book2._id) === true);
+    assert(author1.books.includes(book3._id) === true);
+    assert(author1.name === 'author1_updated');
+
+    assert(book1.authors.includes(author1._id) === false);
+    assert(book2.authors.includes(author1._id) === true);
+    assert(book3.authors.includes(author1._id) === true);
+}
+
+const assertAuthorDelete = async (author) => {
+    const { authors, books } = await fetchCollections();
+
+    const author1 = authors.find(a => a._id.toString() === author._id.toString());
+
+    const book1 = books.find(b => b.title === 'book1');
+    const book2 = books.find(b => b.title === 'book2');
+    const book3 = books.find(b => b.title === 'book3');
+
+    assert(author1 === undefined);
+
+    assert(book1.authors.includes(author._id) === false);
+    assert(book2.authors.includes(author._id) === false);
+    assert(book3.authors.includes(author._id) === false);
+}
